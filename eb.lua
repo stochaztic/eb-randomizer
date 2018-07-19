e4fa88693d16d87cc7dfa4a27338e57672796a08
pickfifteen <pickfifteen@users.noreply.github.com>
2018-07-19 18:15:12 -0400

    Credits: Track damage, instant wins

---
+++ eb.lua
@@ -0,0 +1,13 @@
+
+local pvar
+
+local function main()
+    pvar=memory.readdword(0x7eb608)
+    gui.text(0,2,"Our damage: " .. pvar)
+    pvar=memory.readdword(0x7eb60c)
+    gui.text(0,12,"Enemy damage: " .. pvar)
+    pvar=memory.readword(0x7eb600)
+    gui.text(0,22,"Doors: " .. pvar)
+end
+
+emu.registerbefore(main)
