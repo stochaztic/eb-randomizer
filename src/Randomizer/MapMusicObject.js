c42a6c1a66df0b245124d4c20e8a3f622c73c25e
pickfifteen <pickfifteen@users.noreply.github.com>
2019-02-16 15:24:48 -0500

    Music: Improve music sources, sector changes

--- src/Randomizer/MapMusicObject.js
+++ src/Randomizer/MapMusicObject.js
@@ -11,7 +11,7 @@ class MapMusicObject extends GridTableObject {
 
     static get validMusics() {
         if(this._validMusics !== undefined) return this._validMusics;
-        this._validMusics = new Set(this.every.map(m => m.oldData.music_index))
+        this._validMusics = new Set([4, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 77, 81, 83, 84, 86, 87, 90, 91, 93, 95, 106, 107, 108, 112, 114, 116, 117, 119, 120, 121, 122, 125, 128, 129, 130, 131, 132, 135, 136, 140, 142, 144, 146, 147, 150, 151, 152, 153, 154, 156, 159, 169, 170, 171, 173, 178, 187, 188]);
         if(this.context.specs.flags.w >= 3) {
             EnemyObject.every.forEach(e => this._validMusics.add(e.oldData.music));
         }
@@ -32,6 +32,10 @@ class MapMusicObject extends GridTableObject {
             const musics = Array.from(this.battleMusics);
             EnemyObject.every.forEach(e => e.data.music = this.context.random.choice(musics));
         }
+
+        // Hack map music to not use lookup table
+        this.context.rom.set([0x80, 0x6B], 0x6928);
+
         if(this.context.specs.flags.a) {
             // Map music shuffle - Ancient Cave mode
             const chosenMusics = this.context.random.sample(Array.from(this.validMusics), 9);
@@ -42,6 +46,8 @@ class MapMusicObject extends GridTableObject {
                 mmo.data.music_index = chosenMusics[meo.caveLevel];
                 mmo.mutated = true;
             });
+            // Ignore sector changes for music
+            this.context.rom.set([0x80], 0x5277);
         }
         else {
             // Map music shuffle - Non-Ancient Cave mode
