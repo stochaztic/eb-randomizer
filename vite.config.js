acea100fbcbf64461f89590227ee6b846dcd845b
Chaz <8549883+stochaztic@users.noreply.github.com>
2024-06-04 12:46:03 -0400

    Structural: Bugfix Vite build process

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,9 @@ export default defineConfig(() => {
       outDir: 'build',
     },
     plugins: [react(), nodePolyfills(), arrayPlugin()],
+    worker: {
+      format: "es",
+      plugins: () => [arrayPlugin()],
+    },
   };
 });
